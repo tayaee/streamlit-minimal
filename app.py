ab6653e7ea53fda23835ef452f58401457ea5438
tayaee <soonki.ji@gmail.com>
2024-04-28 22:39:59 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def process(text):
+    return "Hello " + text
+
+if __name__ == "__main__":
+    st.title("Demo app")
+
+    input_text = st.text_input("Prompt:")
+    if st.button("Submit"):
+        output_text = process(input_text)
+        st.write("Response:", output_text)
