64c06cb10b23d3b1db93af4822ca94a27399a227
tayaee <soonki.ji@gmail.com>
2024-05-04 19:07:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def process(text):
-    return "Hello " + text
+    return text.upper()
 
 if __name__ == "__main__":
-    st.title("Demo app v5 from github cs")
+    st.title("Echo Uppercase")
     input_text = st.text_input("Prompt:")
     if st.button("Submit"):
         output_text = process(input_text)
-        st.write("Response:", output_text)
+        st.write(output_text)
